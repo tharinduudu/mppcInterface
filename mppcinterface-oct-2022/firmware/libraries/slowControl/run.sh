5522b27107582d308f6c62e02915d36d5332b2ef
tharinduudu <59080668+tharinduudu@users.noreply.github.com>
2025-09-16 13:36:58 -0400

    Add files via upload

--- mppcinterface-oct-2022/firmware/libraries/slowControl/run.sh
+++ mppcinterface-oct-2022/firmware/libraries/slowControl/run.sh
@@ -1,4 +1,11 @@
-#/bin/sh
-filename=$(date +"Rm415SA-Pi3crm001-E5-Oct5"_"%Y_%m_%d_%H_%M.log")
-echo $filename
-./main $filename
+#!/bin/bash
+# Automatically runs ./main with a timestamped log filename
+
+# Get current date and time in the format YYYY-MM-DD_HH-MM-SS
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Build filename
+filename="Turkey_Obs_E5_${timestamp}.log"
+
+# Run main script with generated filename
+./main "$filename"
